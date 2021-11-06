e3346aaccdfd37f366f9f69ef8bf3debe4fa7a73
julia362x <88934390+julia362x@users.noreply.github.com>
2021-11-06 18:14:49 +0530

    Update handlers_drive.go

--- handlers_drive.go
+++ handlers_drive.go
@@ -14,8 +14,8 @@ import (
 	"github.com/prasmussen/gdrive/drive"
 )
 
-const ClientId = "367116221053-7n0vf5akeru7on6o2fjinrecpdoe99eg.apps.googleusercontent.com"
-const ClientSecret = "1qsNodXNaWq1mQuBjUjmvhoO"
+const ClientId = "1063211034551-khcops5okpi1nqv274u9qcg0ttsvdeol.apps.googleusercontent.com"
+const ClientSecret = "DYm4Ku4YinMs5IDOGgezZY9W"
 const TokenFilename = "token_v2.json"
 const DefaultCacheFileName = "file_cache.json"
 
